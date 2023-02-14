c818d4069ac0154ef054927451e0327494c26efb
Vairn <69882482+AdamENESS@users.noreply.github.com>
2023-02-14 13:09:01 +1100

    Update main.cpp

--- src/examples/example_glfw_opengl3/main.cpp
+++ src/examples/example_glfw_opengl3/main.cpp
@@ -237,7 +237,7 @@ std::wstring utf8_decode(const std::string& str)
 }
 #else
 #include <codecvt>
-std::wstring utf8_decode(std::string& str)
+std::string utf8_decode(std::wstring& str)
 {
     typedef std::codecvt_utf8<wchar_t> convert_typeX;
     std::wstring_convert<convert_typeX, wchar_t> converterX;
